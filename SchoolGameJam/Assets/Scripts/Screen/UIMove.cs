780a6334a01fdc297a440d9af09e69f85bf56493
PotatoCat0325 <hanc02095@gmail.com>
2023-07-22 16:47:25 +0900

    Add UITab

--- SchoolGameJam/Assets/Scripts/Screen/UIMove.cs
+++ SchoolGameJam/Assets/Scripts/Screen/UIMove.cs
@@ -16,9 +16,13 @@ public class UIMove : MonoBehaviour
     [SerializeField] Transform controlKey;
 
     Coroutine coroutine;
-     
-    bool isScreenMove = false;
+    Coroutine coroutine2;
+    Coroutine coroutine3;
     
+    bool isScreenMove = true;
+    bool isUIMoving = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -35,13 +39,63 @@ public class UIMove : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Tab))
         {
+            isUIMoving = true;
             isScreenMove = !isScreenMove;
             if(coroutine != null) StopCoroutine(coroutine);
-            bg.transform.DOMoveY(bgMovePos[(isScreenMove) ? 0 : 1], 1f);
-            ui.transform.DOMoveY(uiMovePos[(isScreenMove) ? 0 : 1], 1f);
-            if (isScreenMove) coroutine = StartCoroutine(IControlKeySet());
-            else controlKey.gameObject.SetActive(false);    
+            if(coroutine2 != null) StopCoroutine(coroutine2);
+            //if (coroutine3 != null) StopCoroutine(coroutine3);
+            // bg.transform.DOMoveY(bgMovePos[(isScreenMove) ? 0 : 1], 1);
+            //ui.transform.DOMoveY(uiMovePos[(isScreenMove) ? 0 : 1], 1f);
+            //coroutine3 = StartCoroutine(IIconSet(1f));
+            coroutine2 = StartCoroutine(IRectSet(1f));
+            coroutine = StartCoroutine(IBGSet(1f));
+            
+            if(!isScreenMove) controlKey.gameObject.SetActive(false);    
         }
+    }
+
+    IEnumerator IIconSet(float time)
+    {
+        yield return new WaitForSeconds(time);
+        controlKey.localPosition = new Vector2(controlKey.localPosition.x, controlKeyPos.position.y);
+        controlKey.gameObject.SetActive(true);
+    }
+
+    IEnumerator IRectSet(float time)
+    {
+        float t = 0f;
+
+        while(t < 1)
+        {
+            t += Time.deltaTime / time;
+            RectTransform rectTransform = ui.GetComponent<RectTransform>();
+            rectTransform.offsetMax = Vector2.Lerp(new Vector2(rectTransform.offsetMax.x, rectTransform.offsetMax.y), new Vector2(rectTransform.offsetMax.x, uiMovePos[(isScreenMove) ? 0 : 1]), t);
+            if(Vector2.Distance(new Vector2(rectTransform.offsetMax.x, rectTransform.offsetMax.y), new Vector2(rectTransform.offsetMax.x, uiMovePos[(isScreenMove) ? 0 : 1])) < 0.5f && isScreenMove) {
+                controlKey.localPosition = new Vector2(controlKey.localPosition.x, controlKeyPos.position.y);
+                controlKey.gameObject.SetActive(true);
+            }
+            yield return null;
+        }
+        isUIMoving = false;
+        //controlKey.localPosition = new Vector2(controlKey.localPosition.x, controlKeyPos.position.y);
+        //if(isScreenMove)
+            //controlKey.gameObject.SetActive(true);
+
+    }
+
+    IEnumerator IBGSet(float time)
+    {
+        float t = 0f;
+
+        while (t < 1)
+        {
+            t += Time.deltaTime / time;
+            //RectTransform rectTransform = ui.GetComponent<RectTransform>();
+            bg.position = Vector2.Lerp(bg.position, new Vector2(bg.position.x, bgMovePos[(isScreenMove) ? 0 : 1]), t);
+            yield return null;
+        }
+       
+
     }
 
     IEnumerator IControlKeySet()
