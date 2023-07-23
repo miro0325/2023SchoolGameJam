c1518aa28f1c2d181c66ac1effd8f0fa435f0fa2
opssoposs <opssoposs@naver.com>
2023-07-23 11:40:32 +0900

    Ui, Sound

--- SchoolGameJam/Assets/Scripts/SoundManager.cs
+++ SchoolGameJam/Assets/Scripts/SoundManager.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static SoundManager instance;
+
+    // 오디오 소스를 저장할 변수
+    private AudioSource audioSource;
+
+    // 배경 음악을 저장할 오디오 클립 변수
+    public AudioClip backgroundMusicClip;
+
+    // 초기화
+    private void Awake()
     {
-        
+        // 사운드 매니저의 인스턴스를 생성하고 자기 자신을 저장
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        // 오디오 소스 컴포넌트 추가
+        audioSource = gameObject.AddComponent<AudioSource>();
+
+        // 배경 음악 로드
+        backgroundMusicClip = Resources.Load<AudioClip>("Bgm/BgMusic");
     }
 
-    // Update is called once per frame
-    void Update()
+    // 배경 음악 재생
+    public void PlayBackgroundMusic(float volume = 1f)
     {
-        
+        audioSource.clip = backgroundMusicClip;
+        audioSource.loop = true;
+        audioSource.volume = volume;
+        audioSource.Play();
     }
 }
--- SchoolGameJam/Assets/Title/SceneFader.cs
+++ SchoolGameJam/Assets/Title/SceneFader.cs
@@ -7,8 +7,11 @@ public class SceneFader : MonoBehaviour
 {
     public Image fadePanel;
     public Text textToBlink;
+    public AudioSource audioSource; // 추가: 페이드 아웃할 사운드를 가진 오디오 소스
     public float fadeDuration = 1.0f;
     public float blinkInterval = 1.0f;
+
+    private bool canFadeOut = false; // 추가: 화면을 터치할 때 페이드 아웃을 시작하기 위한 플래그
 
     private void Start()
     {
@@ -29,9 +32,10 @@ public class SceneFader : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !canFadeOut)
         {
-            FadeToScene("SampleScene");
+            canFadeOut = true;
+            StartCoroutine(FadeOutAndLoadScene("SampleScene"));
         }
     }
 
@@ -42,18 +46,25 @@ public class SceneFader : MonoBehaviour
 
         float timer = 0f;
 
+        // 추가: 처음 등장할 때의 사운드 볼륨 저장
+        float initialVolume = audioSource.volume;
+
         while (timer < fadeDuration)
         {
             float alpha = 1f - (timer / fadeDuration);
             fadePanel.color = new Color(0f, 0f, 0f, alpha);
             timer += Time.deltaTime;
+
+            // 추가: 사운드 볼륨 조절 (처음 등장할 때는 초기 볼륨 유지)
+            
+
             yield return null;
         }
 
         fadePanel.gameObject.SetActive(false);
     }
 
-    private IEnumerator FadeOut(string sceneName)
+    private IEnumerator FadeOutAndLoadScene(string sceneName)
     {
         fadePanel.gameObject.SetActive(true);
         fadePanel.color = new Color(0f, 0f, 0f, 0f);
@@ -65,6 +76,14 @@ public class SceneFader : MonoBehaviour
             float alpha = timer / fadeDuration;
             fadePanel.color = new Color(0f, 0f, 0f, alpha);
             timer += Time.deltaTime;
+
+            // 추가: 사운드 볼륨 조절
+            if (audioSource != null)
+            {
+                float volume = 1f - alpha; // 볼륨은 알파 값의 반대로 설정
+                audioSource.volume = volume;
+            }
+
             yield return null;
         }
 
@@ -88,10 +107,5 @@ public class SceneFader : MonoBehaviour
         }
 
         textToBlink.color = endColor;
-    }
-
-    public void FadeToScene(string sceneName)
-    {
-        StartCoroutine(FadeOut(sceneName));
     }
 }
